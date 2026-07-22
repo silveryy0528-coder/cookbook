e2a9504cc1dd6c3274c87f87926f713bc546fc68
Yan Guo <silveryy0528@gmail.com>
2026-07-22 11:04:37 +0200

    use pylint to keep things tidy

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
+'''
+Create a locally pip installable package for the src directory.
+This is useful for testing and development purposes.
+'''
+
 from setuptools import find_packages, setup
 
 setup(
